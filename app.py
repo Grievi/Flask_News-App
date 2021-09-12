9d8e1e3296a3033d213897615dd28e8433caa88b
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-12 23:42:43 +0300

    define all_articles and assing it a variable a_articles

--- app.py
+++ app.py
@@ -14,7 +14,17 @@ def Index():
                                           category='business',
                                           language='en',
                                           country='us')
+                                          
+    all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      from_param='2017-12-01',
+                                      to='2017-12-12',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
     
     articles = top_headlines['articles']
+    all = all_articles[articles]
 
  
