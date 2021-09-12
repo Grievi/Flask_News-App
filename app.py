b525e24a2dd1cd12db21ecab33fda2d184c2f150
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-13 00:28:27 +0300

    add index.html template and call functions in app.py

--- app.py
+++ app.py
@@ -7,25 +7,15 @@ app = Flask(__name__,template_folder='templates')
 @app.route('/')
 def Index():
     # init
-    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
+    newsapi = NewsApiClient(api_key="3535c8a6cf95459e82bd88c84db6c35b")
     # top headlines
-    top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
-
-    all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
+    top_headlines = newsapi.get_top_headlines(sources="al-jazeera-english")                                     
+    all_articles = newsapi.get_everything(sources="al-jazeera-english")
+                                      
+                                     
     
     articles = top_headlines['articles']
-    a_articles = all_articles[articles]
+    a_articles = all_articles['articles']
 
     news = []
     desc = []
@@ -42,7 +32,7 @@ def Index():
         n_date.append(myarticles['publishedAt'])
         url.append(myarticles['url'])
 
-        toplist = zip(news, desc, img, n_date, url)
+    toplist = zip(news, desc, img, n_date, url)
 
     all_news = []
     all_desc = []
@@ -59,7 +49,7 @@ def Index():
         all_n_date.append(every_article['publishedAt'])
         all_url.append(every_article['url'])
 
-        all = zip(all_news, all_desc, all_img, all_n_date, all_url)
+    all = zip(all_news, all_desc, all_img, all_n_date, all_url)
 
     return render_template('index.html', toplist = toplist, all = all)
 
