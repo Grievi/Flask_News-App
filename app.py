fa9f8393892b5f547cc2af0a205f3229dd48e942
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-13 00:03:08 +0300

    add render the app.py to run and set debug to be true

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def Index():
     articles = top_headlines['articles']
     a_articles = all_articles[articles]
 
-    desc = []
     news = []
+    desc = []
     img = []
     n_date = []
     url = []
@@ -44,19 +44,28 @@ def Index():
 
         toplist = zip(news, desc, img, n_date, url)
 
-    all_desc = []
     all_news = []
+    all_desc = []
     all_img = []
     all_n_date = []
     all_url = []
 
     for j in range(len(a_articles)):
         every_article = a_articles[j]
-        news.append(every_article['title'])
-        desc.append(every_article['description'])
-        img.append(every_article['urlToImage'])
-        n_date.append(every_article['publishedAt'])
-        url.append(every_article['url'])
+
+        all_news.append(every_article['title'])
+        all_desc.append(every_article['description'])
+        all_img.append(every_article['urlToImage'])
+        all_n_date.append(every_article['publishedAt'])
+        all_url.append(every_article['url'])
+
+        all = zip(all_news, all_desc, all_img, all_n_date, all_url)
+
+    return render_template('index.html', toplist = toplist, all = all)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
