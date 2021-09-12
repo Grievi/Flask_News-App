50579b50221ec16270953a336c8664c7a27a4e6e
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-12 23:38:45 +0300

    add top headlines and initialize newsapi

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def Index():
-    newsapi = NewsApiClient
-    (api_key="b0f75ce660c0466a9a98c2478f8abb62")
+    # init
+    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
+    # top headlines
+    top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          category='business',
+                                          language='en',
+                                          country='us')
     
+    articles = top_headlines['articles']
+
  
